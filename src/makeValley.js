6224054e457ba201e400b582c01a1657bfb320ae
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-16 09:26:43 +0200

    3rd and 4th test passed

--- src/makeValley.js
+++ src/makeValley.js
@@ -1,7 +1,37 @@
 function makeValley(arr) {
 
-  if (JSON.stringify(arr) === JSON.stringify([20, 7, 6, 2])) return [20, 6, 2, 7];
+  if (!Array.isArray(arr)) throw new Error('Please provide an Array as an input')
 
-  return [17, 15, 8, 7, 4, 1, 4, 5, 7, 14, 17]}
+  // Find the bottom
+  let bottom = Math.min(...arr)
+  // console.log('Bottom: ' ,bottom)
+
+  // Sort an array from max to min
+  arr.sort((a,b) => b - a );
+  console.log('Sorted: ', arr)
+
+  // First value goest to lwing[0] second to rwing[last]
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = arr.length-1;
+
+  for (let i = 0; i < arr.length; i += 2){
+    result[leftIndex] = arr[i];
+    result[rightIndex] = arr[i+1];
+    leftIndex++;
+    rightIndex--;
+    console.log('i: ', i, 'Result: ', result)
+  }
+
+  console.log('Result: ', result)
+
+  if (result.length % 2 !== 0){
+    let middle = Math.floor(result.length / 2);
+    result[middle] = bottom;
+  }
+
+  return result;
+
+}
 
 module.exports = makeValley;
--- src/makeValley.test.js
+++ src/makeValley.test.js
@@ -8,4 +8,13 @@ describe('makeValley', () => {
   it('[20, 7, 6, 2], should return [20, 6, 2, 7]', () => {
     expect(makeValley([20, 7, 6, 2])).toStrictEqual([20, 6, 2, 7]);
   });
+
+  it('[14, 10, 8], should return [14, 8, 10]', () => {
+    expect(makeValley([14, 10, 8])).toStrictEqual([14, 8, 10]);
+  });
+
+  it('[20, 20, 16, 14, 12, 12, 11, 10, 3, 2], should return [20, 16, 12, 11, 3, 2, 10, 12, 14, 20]', () => {
+    expect(makeValley([20, 20, 16, 14, 12, 12, 11, 10, 3, 2])).toStrictEqual([20, 16, 12, 11, 3, 2, 10, 12, 14, 20]);
+  });
+
 });
