44e8d9a683970a6fa28a97124b8724809b04209c
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-16 10:39:26 +0200

    Trying approach with left and right wing

--- src/makeValley.js
+++ src/makeValley.js
@@ -2,35 +2,54 @@ function makeValley(arr) {
 
   if (!Array.isArray(arr)) throw new Error('Please provide an Array as an input')
 
-  // Find the bottom
-  let bottom = Math.min(...arr)
-  // console.log('Bottom: ' ,bottom)
+  // // Find the bottom
+  // let bottom = Math.min(...arr)
+  // // console.log('Bottom: ' ,bottom)
 
-  // Sort an array from max to min
-  arr.sort((a,b) => b - a );
+  // // Sort an array from max to min
+  // arr.sort((a,b) => b - a );
+
+  // // First value goest to lwing[0] second to rwing[last]
+  // let result = [];
+  // let leftIndex = 0;
+  // let rightIndex = arr.length-1;
+
+  // for (let i = 0; i < arr.length; i += 2){
+  //   result[leftIndex] = arr[i];
+  //   result[rightIndex] = arr[i+1];
+  //   leftIndex++;
+  //   rightIndex--;
+  // }
+
+  // if (result.length % 2 !== 0){
+  //   let middle = Math.floor(result.length / 2);
+  //   result[middle] = bottom;
+  // }
+
+  // return result;
+
+
+  // Sort array 
+  arr.sort((a,b) => b - a);
   console.log('Sorted: ', arr)
 
-  // First value goest to lwing[0] second to rwing[last]
-  let result = [];
-  let leftIndex = 0;
-  let rightIndex = arr.length-1;
+  // Create a leftWing and rightWing
+  let leftWing = [];
+  let rightWing = [];
 
-  for (let i = 0; i < arr.length; i += 2){
-    result[leftIndex] = arr[i];
-    result[rightIndex] = arr[i+1];
-    leftIndex++;
-    rightIndex--;
-    console.log('i: ', i, 'Result: ', result)
+  // Loop the sorted array and allocate respective values
+  for (let i = 0; i < arr.length; i++){
+    if (i % 2 !== 0){
+      rightWing.push(arr[i])
+    }
   }
 
-  console.log('Result: ', result)
+  // Reverse right array
 
-  if (result.length % 2 !== 0){
-    let middle = Math.floor(result.length / 2);
-    result[middle] = bottom;
-  }
 
-  return result;
+  // Concat the arrays and return them
+
+
 
 }
 
