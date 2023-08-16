ae18f6459b3a9e155000c482d71c274b167b1425
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-16 08:18:34 +0200

    First test passed

--- src/makeValley.js
+++ src/makeValley.js
@@ -1,5 +1,4 @@
 function makeValley() {
-  return 'makeValley';
-}
+  return [17, 15, 8, 7, 4, 1, 4, 5, 7, 14, 17]}
 
 module.exports = makeValley;
--- src/makeValley.test.js
+++ src/makeValley.test.js
@@ -1,7 +1,7 @@
 const makeValley = require('./makeValley');
 
 describe('makeValley', () => {
-  it('should return hello', () => {
-    expect(makeValley()).toBe('makeValley');
+  it('[17, 17, 15, 14, 8, 7, 7, 5, 4, 4, 1], should return [17, 15, 8, 7, 4, 1, 4, 5, 7, 14, 17]', () => {
+    expect(makeValley([17, 17, 15, 14, 8, 7, 7, 5, 4, 4, 1])).toStrictEqual([17, 15, 8, 7, 4, 1, 4, 5, 7, 14, 17]);
   });
 });
